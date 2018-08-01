75102c37e4293d52d120976df1449525d6ade2df
Andrew Bannerman <bannerman1985@gmail.com>
2018-08-01 18:59:58 -0500

    4-2 Decycler Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,9 +9,11 @@ end
 # count 0
 count_nans(x) = sum(isnan.(x))
 
+######################################################################
 # Validate against John Ehlers Original TradeStation Easylanguage code
+######################################################################
 
-# SuperSmoother Test
+# 3-3 SuperSmoother Test
 test = CSV.read("C:/Users/Andrew.Bannerman/.julia/v0.6/MarketCycles/test/test_3-3_Supersmoother.csv")
 close = Float64.(test[:Close])
 super_smoother_benchmark = Float64.(test[:Ten_Period_Supersmoother])
@@ -19,4 +21,12 @@ Super_Smoother = SuperSmoother(close,n=10)
 Super_Smoother = round.(Super_Smoother,2) # round same as tradestation output
 valid = ifelse.(Super_Smoother .== super_smoother_benchmark,1,0)
 valid = valid[28:length(valid)] # remove indicator lead in period
+@test sum(valid) == length(valid)
+
+# 4-2 Decycler Test
+test = CSV.read("C:/Users/Andrew.Bannerman/.julia/v0.6/MarketCycles/test/test_4-1_Decycler.csv")
+decycler_benchmark = Float64.(test[:Sixty_Period_Decycler])
+decycler = Decycler(close, n=60)
+decycler = round.(decycler,2) # round same as tradestation output
+valid = ifelse.(decycler .== decycler_benchmark,1,0)
 @test sum(valid) == length(valid)
