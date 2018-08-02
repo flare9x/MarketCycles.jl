f90d0495d265b45abd49f3088cc98fa10aa7068f
Andrew Bannerman <bannerman1985@gmail.com>
2018-08-02 06:52:47 -0500

    run tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,6 @@ else
     using Test
 end
 
-# count 0
-count_nans(x) = sum(isnan.(x))
-
 ######################################################################
 # Validate against John Ehlers Original TradeStation Easylanguage code
 ######################################################################
@@ -23,10 +20,18 @@ valid = ifelse.(Super_Smoother .== super_smoother_benchmark,1,0)
 valid = valid[28:length(valid)] # remove indicator lead in period
 @test sum(valid) == length(valid)
 
-# 4-2 Decycler Test
+# 4-1 Decycler Test
 test = CSV.read("C:/Users/Andrew.Bannerman/.julia/v0.6/MarketCycles/test/test_4-1_Decycler.csv")
 decycler_benchmark = Float64.(test[:Sixty_Period_Decycler])
 decycler = Decycler(close, n=60)
 decycler = round.(decycler,2) # round same as tradestation output
 valid = ifelse.(decycler .== decycler_benchmark,1,0)
+@test sum(valid) == length(valid)
+
+# 4-2 - Decycle Oscillator test
+test = CSV.read("C:/Users/Andrew.Bannerman/.julia/v0.6/MarketCycles/test/test_4-2_Decycle_Oscillator.csv")
+decycler_osc_benchmark = Float64.(test[:thirty_sixty_decycle_osc])
+decycler_osc = Decycle_OSC(close, n1=30,n2=60)
+decycler_osc = round.(decycler,2) # round same as tradestation output
+valid = ifelse.(decycler_osc .== decycler_osc_benchmark,1,0)
 @test sum(valid) == length(valid)
