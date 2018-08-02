7b72a0cab8b49c7619cb670719d02311ce8adae3
Andrew Bannerman <bannerman1985@gmail.com>
2018-08-02 13:05:54 -0500

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,7 +12,8 @@ end
 
 # 3-3 SuperSmoother Test
 @testset "Ehlers Tests" begin
-test = CSV.read("C:/Users/Andrew.Bannerman/.julia/v0.6/MarketCycles/test/test_3-3_Supersmoother.csv")
+filename = joinpath(dirname(@__FILE__), "test_3-3_Supersmoother.csv")
+test = CSV.read( filename)
 close = Float64.(test[:Close])
 super_smoother_benchmark = Float64.(test[:Ten_Period_Supersmoother])
 Super_Smoother = SuperSmoother(close,n=10)
@@ -22,18 +23,21 @@ valid = valid[28:length(valid)] # remove indicator lead in period
 @test sum(valid) == length(valid)
 
 # 4-1 Decycler Test
-test = CSV.read("C:/Users/Andrew.Bannerman/.julia/v0.6/MarketCycles/test/test_4-1_Decycler.csv")
+filename = joinpath(dirname(@__FILE__), "test_4-1_Decycler.csv")
+test = CSV.read( filename)
 decycler_benchmark = Float64.(test[:Sixty_Period_Decycler])
 decycler = Decycler(close, n=60)
 decycler = round.(decycler,2) # round same as tradestation output
 valid = ifelse.(decycler .== decycler_benchmark,1,0)
 @test sum(valid) == length(valid)
-end
 
 # 4-2 - Decycle Oscillator test
-test = CSV.read("C:/Users/Andrew.Bannerman/.julia/v0.6/MarketCycles/test/test_4-2_Decycle_Oscillator.csv")
+filename = joinpath(dirname(@__FILE__), "test_4-2_Decycle_Oscillator.csv")
+test = CSV.read( filename)
 decycler_osc_benchmark = Float64.(test[:thirty_sixty_decycle_osc])
 decycler_osc = Decycle_OSC(close, n1=30,n2=60)
 decycler_osc = round.(decycler,2) # round same as tradestation output
 valid = ifelse.(decycler_osc .== decycler_osc_benchmark,1,0)
+valid
 @test sum(valid) == length(valid)
+end
