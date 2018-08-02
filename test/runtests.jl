7d9b2b3648f6ddd8f61052e630536a5a6c000227
femtotrader <femto.trader@gmail.com>
2018-08-02 21:36:27 +0200

    Update runtests.jl
    Better indent
    Nested testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using MarketCycles
 using CSV
+
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
 else
@@ -11,33 +12,39 @@ end
 ######################################################################
 
 # 3-3 SuperSmoother Test
-@testset "Ehlers Tests" begin
-filename = joinpath(dirname(@__FILE__), "test_3-3_Supersmoother.csv")
-test = CSV.read( filename)
-close = Float64.(test[:Close])
-super_smoother_benchmark = Float64.(test[:Ten_Period_Supersmoother])
-Super_Smoother = SuperSmoother(close,n=10)
-Super_Smoother = round.(Super_Smoother,2) # round same as tradestation output
-valid = ifelse.(Super_Smoother .== super_smoother_benchmark,1,0)
-valid = valid[28:length(valid)] # remove indicator lead in period
-@test sum(valid) == length(valid)
+@testset "Ehlers" begin
+    @testset "3-3 SuperSmoother" begin
+        filename = joinpath(dirname(@__FILE__), "test_3-3_Supersmoother.csv")
+        test = CSV.read( filename)
+        close = Float64.(test[:Close])
+        super_smoother_benchmark = Float64.(test[:Ten_Period_Supersmoother])
+        Super_Smoother = SuperSmoother(close,n=10)
+        Super_Smoother = round.(Super_Smoother,2) # round same as tradestation output
+        valid = ifelse.(Super_Smoother .== super_smoother_benchmark,1,0)
+        valid = valid[28:length(valid)] # remove indicator lead in period
+        @test sum(valid) == length(valid)
+    end
 
-# 4-1 Decycler Test
-filename = joinpath(dirname(@__FILE__), "test_4-1_Decycler.csv")
-test = CSV.read( filename)
-decycler_benchmark = Float64.(test[:Sixty_Period_Decycler])
-decycler = Decycler(close, n=60)
-decycler = round.(decycler,2) # round same as tradestation output
-valid = ifelse.(decycler .== decycler_benchmark,1,0)
-@test sum(valid) == length(valid)
+    @testset "4-1 Decycler" begin
+        filename = joinpath(dirname(@__FILE__), "test_4-1_Decycler.csv")
+        test = CSV.read( filename)
+        decycler_benchmark = Float64.(test[:Sixty_Period_Decycler])
+        decycler = Decycler(close, n=60)
+        decycler = round.(decycler,2) # round same as tradestation output
+        valid = ifelse.(decycler .== decycler_benchmark,1,0)
+        @test sum(valid) == length(valid)
+    end
+
+    #=
+    @testset "4-2 - Decycle Oscillator" begin
+        filename = joinpath(dirname(@__FILE__), "test_4-2_Decycle_Oscillator.csv")
+        test = CSV.read( filename)
+        decycler_osc_benchmark = Float64.(test[:thirty_sixty_decycle_osc])
+        decycler_osc = Decycle_OSC(close, n1=30,n2=60)
+        decycler_osc = round.(decycler,2) # round same as tradestation output
+        valid = ifelse.(decycler_osc .== decycler_osc_benchmark,1,0)
+        valid
+        @test sum(valid) == length(valid)
+    end
+    =#
 end
-
-# 4-2 - Decycle Oscillator test
-#filename = joinpath(dirname(@__FILE__), "test_4-2_Decycle_Oscillator.csv")
-#test = CSV.read( filename)
-#decycler_osc_benchmark = Float64.(test[:thirty_sixty_decycle_osc])
-#decycler_osc = Decycle_OSC(close, n1=30,n2=60)
-#decycler_osc = round.(decycler,2) # round same as tradestation output
-#valid = ifelse.(decycler_osc .== decycler_osc_benchmark,1,0)
-#valid
-#@test sum(valid) == length(valid)
