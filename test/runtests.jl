8123d25ae6622c7ab978a6415f2638008e384c44
Andrew Bannerman <bannerman1985@gmail.com>
2018-08-02 14:11:02 -0500

    test!!!!

--- test/runtests.jl
+++ test/runtests.jl
@@ -33,11 +33,11 @@ valid = ifelse.(decycler .== decycler_benchmark,1,0)
 end
 
 # 4-2 - Decycle Oscillator test
-filename = joinpath(dirname(@__FILE__), "test_4-2_Decycle_Oscillator.csv")
-test = CSV.read( filename)
-decycler_osc_benchmark = Float64.(test[:thirty_sixty_decycle_osc])
-decycler_osc = Decycle_OSC(close, n1=30,n2=60)
-decycler_osc = round.(decycler,2) # round same as tradestation output
-valid = ifelse.(decycler_osc .== decycler_osc_benchmark,1,0)
-valid
+#filename = joinpath(dirname(@__FILE__), "test_4-2_Decycle_Oscillator.csv")
+#test = CSV.read( filename)
+#decycler_osc_benchmark = Float64.(test[:thirty_sixty_decycle_osc])
+#decycler_osc = Decycle_OSC(close, n1=30,n2=60)
+#decycler_osc = round.(decycler,2) # round same as tradestation output
+#valid = ifelse.(decycler_osc .== decycler_osc_benchmark,1,0)
+#valid
 #@test sum(valid) == length(valid)
