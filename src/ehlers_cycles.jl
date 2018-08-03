c2cf5276119cf2820b7304cc3c2a771267cecbc0
Bannerman <Andrew.Bannerman@shell.com>
2018-08-02 21:03:41 -0500

    runtests up to 7-3 roofing filter indicator

--- src/ehlers_cycles.jl
+++ src/ehlers_cycles.jl
@@ -12,17 +12,17 @@
 Super Smoother - Equation 3-3
 """
 function SuperSmoother(x::Array{Float64}; n::Int64=10)::Array{Float64}
-a = exp(-1.414*3.14159 / n)
-b = 2 * a * cosd(1.414 * 180 / n)
-c2 = b
-c3 = -a * a
-c1 = 1 - c2 - c3
-@assert n<size(x,1) && n>0 "Argument n out of bounds."
-Super = zeros(x)
- @inbounds for i = 3:length(x)
-Super[i] = c1 * (x[i] + x[i-1]) / 2 + c2 * Super[i-1] + c3 * Super[i-2]
-end
-return Super
+    @assert n<size(x,1) && n>0 "Argument n out of bounds."
+    a = exp(-1.414*3.14159 / n)
+    b = 2 * a * cosd(1.414 * 180 / n)
+    c2 = b
+    c3 = -a * a
+    c1 = 1 - c2 - c3
+    Super = zeros(x)
+    @inbounds for i = 3:length(x)
+        Super[i] = c1 * (x[i] + x[i-1]) / 2 + c2 * Super[i-1] + c3 * Super[i-2]
+    end
+    return Super
 end
 
 """
@@ -41,274 +41,284 @@ function Decycler(x::Array{Float64}; n::Int64=60)::Array{Float64}
     return Decycle
 end
 
-@doc """
+"""
+    Decycle_OSC(x::Array{Float64}; n1::Int64=30, n2::Int64=60)::Array{Float64}
+
 Decycle Oscillator - Equation 4-2
-`Decycle_OSC(x::Array{Float64}; n1::Int64=30, n2::Int64=60)::Array{Float64}`
 """
 function Decycle_OSC(x::Array{Float64}; n1::Int64=30, n2::Int64=60)::Array{Float64}
-        @assert n2<size(x,1) && n2>0 "Argument n out of bounds."
-alpha1 = (cosd(.707*360 / n1) + sind(.707*360 / n1) - 1) / cosd(.707*360 / n1)
-alpha2 = (cosd(.707*360 / n2) + sind(.707*360 / n2) - 1) / cosd(.707*360 / n2)
-HP1 = zeros(x)
-HP2 = zeros(x)
-Decycle_OSC = zeros(x)
-@inbounds for i in 3:length(x)
-HP1[i] = (1 - alpha1 / 2)*(1 - alpha1 / 2)*(x[i] - 2*x[i-1] + x[i-2]) + 2*(1 - alpha1)*HP1[i-1] - (1 - alpha1)* (1 - alpha1)*HP1[i-2]
-HP2[i] = (1 - alpha2 / 2)*(1 - alpha2 / 2)*(x[i] - 2*x[i-1] + x[i-2]) + 2*(1 - alpha2)*HP2[i-1] - (1 - alpha2)*(1 - alpha2)*HP2[i-2]
-end
-Decycle_OSC .= HP2 .- HP1
-return Decycle_OSC
+    @assert n2<size(x,1) && n2>0 "Argument n out of bounds."
+    alpha1 = (cosd(.707*360 / n1) + sind(.707*360 / n1) - 1) / cosd(.707*360 / n1)
+    alpha2 = (cosd(.707*360 / n2) + sind(.707*360 / n2) - 1) / cosd(.707*360 / n2)
+    HP1 = zeros(x)
+    HP2 = zeros(x)
+    Decycle_OSC = zeros(x)
+    @inbounds for i in 3:length(x)
+        HP1[i] = (1 - alpha1 / 2)*(1 - alpha1 / 2)*(x[i] - 2*x[i-1] + x[i-2]) + 2*(1 - alpha1)*HP1[i-1] - (1 - alpha1)* (1 - alpha1)*HP1[i-2]
+        HP2[i] = (1 - alpha2 / 2)*(1 - alpha2 / 2)*(x[i] - 2*x[i-1] +x[i-2]) + 2*(1 - alpha2)*HP2[i-1] - (1 - alpha2)*(1 - alpha2)*HP2[i-2]
+    end
+    Decycle_OSC .= HP2 .- HP1
+    return Decycle_OSC
 end
 
-@doc """
+"""
+    BandPassFilter(x::Array{Float64}; n::Int64=30, bandwidth::Float64=.3)::Array{Float64}
+
 Band Pass Filter - Equation 5-1
-`BandPassFilter(x::Array{Float64}; n::Int64=30, bandwidth::Float64=.3)::Array{Float64}`
 """
 function BandPassFilter(x::Array{Float64}; n::Int64=30, bandwidth::Float64=.3)::Array{Float64}
-            @assert n<size(x,1) && n>0 "Argument n out of bounds."
-alpha2 = (cosd(.25*bandwidth*360 / n) + sind(.25*bandwidth*360 / n) - 1) / cosd(.25*bandwidth*360 /n)
-beta1 = cosd(360 / n);
-gamma1 = 1 / cosd(360*bandwidth / n)
-alpha1 = gamma1 - sqrt(gamma1*gamma1 - 1)
-HP = zeros(x)
-BP = zeros(x)
-@inbounds for i in 3:length(x)
-HP[i] = (1 + alpha2 / 2)*(x[i] - x[i-1]) + (1- alpha2)*HP[i-1]
-BP[i] = .5*(1 - alpha1)*(HP[i] - HP[i-2]) + beta1*(1 + alpha1)*BP[i-1] - alpha1*BP[i-2]
-end
-# Signal
-Signal = zeros(x)
-Peak = zeros(x)
-@inbounds for i in 2:length(BP)
-Peak[i] = .991*Peak[i-1]
-if abs(BP[i]) > Peak[i]
-Peak[i] = abs(BP[i])
-    Signal[i] = BP[i] / Peak[i]
-else
+    @assert n<size(x,1) && n>0 "Argument n out of bounds."
+    alpha2 = (cosd(.25*bandwidth*360 / n) + sind(.25*bandwidth*360 / n) - 1) / cosd(.25*bandwidth*360 /n)
+    beta1 = cosd(360 / n);
+    gamma1 = 1 / cosd(360*bandwidth / n)
+    alpha1 = gamma1 - sqrt(gamma1*gamma1 - 1)
+    HP = zeros(x)
+    BP = zeros(x)
+    @inbounds for i in 3:length(x)
+    HP[i] = (1 + alpha2 / 2)*(x[i] - x[i-1]) + (1- alpha2)*HP[i-1]
+    BP[i] = .5*(1 - alpha1)*(HP[i] - HP[i-2]) + beta1*(1 + alpha1)*BP[i-1] - alpha1*BP[i-2]
+    end
+    # Signal
+    Signal = zeros(x)
+    Peak = zeros(x)
+    @inbounds for i in 2:length(BP)
+        Peak[i] = .991*Peak[i-1]
+        if abs(BP[i]) > Peak[i]
+            Peak[i] = abs(BP[i])
+        if Peak[i] != 0
+            Signal[i] = BP[i] / Peak[i]
+        end
+        else
         Signal[i] = BP[i] / Peak[i]
-end
-end
+    end
+    end
 
-# Replace Nan to 0
-@inbounds for i in 1:length(Signal)
+    # Replace Nan to 0
+    @inbounds for i in 1:length(Signal)
     if isnan(Signal[i]) == 1
         Signal[i] = 0.0
     else
         Signal[i] = Signal[i]
     end
+    end
+
+    # Trigger
+    alpha2 = (cosd(1.5*bandwidth*360 / n) + sind(1.5*bandwidth*360 / n) - 1) / cosd(1.5*bandwidth*360 /n)
+    BP_Trigger = zeros(x)
+    i=1
+    @inbounds for i = 2:length(x)
+        BP_Trigger[i] = (1 + alpha2 / 2)*(Signal[i] - Signal[i-1]) +(1 -alpha2)*BP_Trigger[i-1]
+    end
+    return BP_Trigger
 end
 
-# Trigger
-alpha2 = (cosd(1.5*bandwidth*360 / n) + sind(1.5*bandwidth*360 / n) - 1) / cosd(1.5*bandwidth*360 /n)
-BP_Trigger = zeros(x)
-i=1
-@inbounds for i = 2:length(x)
-BP_Trigger[i] = (1 + alpha2 / 2)*(Signal[i] - Signal[i-1]) +(1 -alpha2)*BP_Trigger[i-1]
-end
-return BP_Trigger
-end
-
-
-@doc """
-Hurst Coefficient - Equation 6-1
-`HurstCoefficient(x::Array{Float64}; n::Int64=30,)::Array{Float64}`
 """
-function HurstCoefficient(x::Array{Float64}; n::Int64=30,)::Array{Float64}
-    @assert n<size(x,1) && n>0 "Argument n out of bounds."
-        @assert iseven(n) "n must be an even number."
-half_n = Int64(n/2)
-a1 = exp(-1.414*3.14159 / 20)  # smoothes by 20 period same as equation 3-3- may wish to make this a argument in the function?
-b1 = 2*a1*cosd(1.414*180 / 20) # smoothes by 20 period same as equation 3-3- may wish to make this a argument in the function?
-c2 = b1
-c3 = -a1*a1
-c1 = 1 - c2 - c3
-# Find rolling maximum and minimum
-HH = zeros(x)
-LL = zeros(x)
-N3 = zeros(x)
-@inbounds for i = n:size(x,1)
-                HH[i] = maximum(x[i-n+1:i])
-                LL[i] = minimum(x[i-n+1:i])
-                N3[i] = (HH[i] - LL[i]) / n
-            end
-# Rolling min and max half of n
-HH = zeros(x)
-LL = zeros(x)
-N1 = zeros(x)
-@inbounds for i = half_n:size(x,1)
-                HH[i] = maximum(x[i-half_n+1:i])
-                LL[i] = minimum(x[i-half_n+1:i])
-                N1[i] = (HH[i] - LL[i]) / half_n
-            end
+    urstCoefficient(x::Array{Float64}; n::Int64=30, Smooth::Int64=20)::Array{Float64}
 
-# Set trailing close half of n
-HH = [fill(0,half_n); x[1:length(x)-half_n]]
-LL = [fill(0,half_n); x[1:length(x)-half_n]]
-HH_out = zeros(x)
-LL_out = zeros(x)
-N2 = zeros(x)
-@inbounds for i = half_n:size(x,1)
+Hurst Coefficient - Equation 6-1
+"""
+function HurstCoefficient(x::Array{Float64}; n::Int64=30, Smooth::Int64=20)::Array{Float64}
+    @assert n<size(x,1) && n>0 "Argument n out of bounds."
+    @assert iseven(n) "n must be an even number."
+    # Smooth with a Super Smoother Filter from equation 3-3
+    half_n = Int64(n/2)
+    a1 = exp(-1.414*3.14159 / Smooth)
+    b1 = 2*a1*cosd(1.414*180 / Smooth)
+    c2 = b1
+    c3 = -a1*a1
+    c1 = 1 - c2 - c3
+    # Find rolling maximum and minimum
+    HH = zeros(x)
+    LL = zeros(x)
+    N3 = zeros(x)
+    @inbounds for i = n:size(x,1)
+    HH[i] = maximum(x[i-n+1:i])
+    LL[i] = minimum(x[i-n+1:i])
+    N3[i] = (HH[i] - LL[i]) / n
+    end
+    # Rolling min and max half of n
+    HH = zeros(x)
+    LL = zeros(x)
+    N1 = zeros(x)
+    @inbounds for i = half_n:size(x,1)
+    HH[i] = maximum(x[i-half_n+1:i])
+    LL[i] = minimum(x[i-half_n+1:i])
+    N1[i] = (HH[i] - LL[i]) / half_n
+    end
+    # Set trailing close half of n
+    HH = [fill(0,half_n); x[1:length(x)-half_n]]
+    LL = [fill(0,half_n); x[1:length(x)-half_n]]
+    HH_out = zeros(x)
+    LL_out = zeros(x)
+    N2 = zeros(x)
+    @inbounds for i = half_n:size(x,1)
     HH_out[i] = maximum(HH[i-half_n+1:i])
     LL_out[i] = minimum(LL[i-half_n+1:i])
     N2[i] = (HH_out[i] - LL_out[i])/(half_n)
-end
-
-# Hurst
-Dimen = zeros(x)
-Hurst = zeros(x)
-SmoothHurst = zeros(x)
-@inbounds for i = 3:size(x,1)
-if N1[i] > 0 && N2[i] > 0 && N3[i] > 0
+    end
+    # Hurst
+    Dimen = zeros(x)
+    Hurst = zeros(x)
+    SmoothHurst = zeros(x)
+    @inbounds for i = 3:size(x,1)
+    if N1[i] > 0 && N2[i] > 0 && N3[i] > 0
     Dimen[i] = .5*((log(N1[i]+ N2[i]) - log(N3[i])) / log(2) + Dimen[i-1])
-Hurst[i] = 2 - Dimen[i]
-SmoothHurst[i] = c1*(Hurst[i] + Hurst[i-1]) / 2 + c2*SmoothHurst[i-1]+ c3*SmoothHurst[i-2];
-end
-end
-return SmoothHurst
+    Hurst[i] = 2 - Dimen[i]
+    SmoothHurst[i] = c1*(Hurst[i] + Hurst[i-1]) / 2 + c2*SmoothHurst[i-1]+ c3*SmoothHurst[i-2];
+    end
+    end
+    return SmoothHurst
 end
 
-@doc """
+"""
+    HPLPRoofingFilter(x::Array{Float64}; HPPeriod::Int64=48, Smooth::Int64=10)::Array{Float64}
+
 HP LP Roofing Filter - Equation 7-1
-`HPLPRoofingFilter(x::Array{Float64}; n::Int64=30,)::Array{Float64}`
 """
-function HPLPRoofingFilter(x::Array{Float64})::Array{Float64}
-        @assert n<size(x,1) && n>0 "Argument n out of bounds."
-# Highpass filter cyclic components whose periods are shorter than 48 bars
-alpha1 = (cosd(360 / 48) + sind(360 / 48) - 1) / cosd(360 / 48)
-HP = zeros(x)
-@inbounds for i = 2:size(x,1)
+function HPLPRoofingFilter(x::Array{Float64}; HPPeriod::Int64=48, Smooth::Int64=10)::Array{Float64}
+    @assert HPPeriod<size(x,1) && HPPeriod>0 "Argument n out of bounds."
+    # Highpass filter cyclic components whose periods are shorter than 48 bars
+    alpha1 = (cosd(360 / HPPeriod) + sind(360 / HPPeriod) - 1) / cosd(360 / HPPeriod)
+    HP = zeros(x)
+    @inbounds for i = 2:size(x,1)
     HP[i] = (1 - alpha1 / 2)*(x[i] - x[i-1]) + (1 - alpha1)*HP[i-1]
-end
-# Smooth with a Super Smoother Filter from equation 3-3
-a1 = exp(-1.414*3.14159 / 10)  # may wish to make this an argument in function
-b1 = 2*a1*cosd(1.414*180 / 10) # may wish to make this an argument in function
-c2 = b1
-c3 = -a1*a1
-c1 = 1 - c2 - c3
-LP_HP_Filt = zeros(x)
-@inbounds for i = 3:size(x,1)
-LP_HP_Filt[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*LP_HP_Filt[i-1] + c3*LP_HP_Filt[i-2]
-end
-return LP_HP_Filt
+    end
+    # Smooth with a Super Smoother Filter from equation 3-3
+    a1 = exp(-1.414*3.14159 / Smooth)  # may wish to make this an argument in function
+    b1 = 2*a1*cosd(1.414*180 / Smooth) # may wish to make this an argument in function
+    c2 = b1
+    c3 = -a1*a1
+    c1 = 1 - c2 - c3
+    LP_HP_Filt = zeros(x)
+    @inbounds for i = 3:size(x,1)
+        LP_HP_Filt[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*LP_HP_Filt[i-1] + c3*LP_HP_Filt[i-2]
+    end
+    return LP_HP_Filt
 end
 
-@doc """
-Zero Mean Roofing Filter - Equation 7-2
-P0 = Lag 0
-`ZeroMeanRoofingFilterP0(x::Array{Float64})::Array{Float64}`
 """
-function ZeroMeanRoofingFilterP0(x::Array{Float64})::Array{Float64}
-            @assert n<size(x,1) && n>0 "Argument n out of bounds."
-# Highpass filter cyclic components whose periods are shorter than 48 bars
-alpha1 = (cosd(360 / 48) + sind(360 / 48) - 1) /cosd(360 / 48)
-HP = zeros(x)
-@inbounds for i = 2:size(data1_c,1)
-HP[i] = (1 - alpha1 / 2)*(x[i] - x[i-1]) +(1 - alpha1)*HP[i-1]
-end
-#Smooth with a Super Smoother Filter from equation 3-3
-a1 = exp(-1.414*3.14159 / 10)
-b1 = 2*a1*cosd(1.414*180 / 10)
-c2 = b1
-c3 = -a1*a1
-c1 = 1 - c2 - c3
-Zero_Mean_Filt = zeros(x)
-Zero_Mean_Filt2 = zeros(x)
-@inbounds for i = 3:size(x,1)
-Zero_Mean_Filt[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*Zero_Mean_Filt[i-1] + c3*Zero_Mean_Filt[i-2]
-Zero_Mean_Filt2[i] = (1 - alpha1 / 2)*(Filt[i] - Filt[i-1]) + (1 - alpha1)*Filt2[i-1]
-end
-return Zero_Mean_Filt
-end
+    ZeroMeanRoofingFilterK0(x::Array{Float64}; HPPeriod::Int64=48, Smooth::Int64=10)::Array{Float64}
 
-@doc """
-Zero Mean Roofing Filter - Equation 7-2
-P1 = Lag 1
-`ZeroMeanRoofingFilterP0(x::Array{Float64})::Array{Float64}`
+Zero Mean Roofing Filter - Lag 0 - Equation 7-2
+K0 = Lag 0
+# Lag 0 Is Most Responsive
+# Ehlers describes using Lag 0 and Lag 1 cross overs/unders as a signal trigger for buying / selling
 """
-function ZeroMeanRoofingFilterP1(x::Array{Float64})::Array{Float64}
-            @assert n<size(x,1) && n>0 "Argument n out of bounds."
-# Highpass filter cyclic components whose periods are shorter than 48 bars
-alpha1 = (cosd(360 / 48) + sind(360 / 48) - 1) /cosd(360 / 48)
-HP = zeros(x)
-@inbounds for i = 2:size(data1_c,1)
-HP[i] = (1 - alpha1 / 2)*(x[i] - x[i-1]) +(1 - alpha1)*HP[i-1]
-end
-#Smooth with a Super Smoother Filter from equation 3-3
-a1 = exp(-1.414*3.14159 / 10)
-b1 = 2*a1*cosd(1.414*180 / 10)
-c2 = b1
-c3 = -a1*a1
-c1 = 1 - c2 - c3
-Zero_Mean_Filt = zeros(x)
-Zero_Mean_Filt2 = zeros(x)
-@inbounds for i = 3:size(x,1)
-Zero_Mean_Filt[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*Zero_Mean_Filt[i-1] + c3*Zero_Mean_Filt[i-2]
-Zero_Mean_Filt2[i] = (1 - alpha1 / 2)*(Zero_Mean_Filt[i] - Zero_Mean_Filt[i-1]) + (1 - alpha1)*Zero_Mean_Filt2[i-1]
-end
-return Zero_Mean_Filt2
+function ZeroMeanRoofingFilterK0(x::Array{Float64}; HPPeriod::Int64=48, Smooth::Int64=10)::Array{Float64}
+    @assert HPPeriod<size(x,1) && HPPeriod>0 "Argument n out of bounds."
+    # Highpass filter cyclic components whose periods are shorter than 48 bars
+    alpha1 = (cosd(360 / HPPeriod) + sind(360 / HPPeriod) - 1) /cosd(360 / HPPeriod)
+    HP = zeros(x)
+    @inbounds for i = 2:size(x,1)
+        HP[i] = (1 - alpha1 / 2)*(x[i] - x[i-1]) +(1 - alpha1)*HP[i-1]
+    end
+    #Smooth with a Super Smoother Filter from equation 3-3
+    a1 = exp(-1.414*3.14159 / Smooth)
+    b1 = 2*a1*cosd(1.414*180 / Smooth)
+    c2 = b1
+    c3 = -a1*a1
+    c1 = 1 - c2 - c3
+    Zero_Mean_Filt = zeros(x)
+    Zero_Mean_Filt2 = zeros(x)
+    @inbounds for i = 3:size(x,1)
+        Zero_Mean_Filt[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*Zero_Mean_Filt[i-1] + c3*Zero_Mean_Filt[i-2]
+        Zero_Mean_Filt2[i] = (1 - alpha1 / 2)*(Zero_Mean_Filt[i] - Zero_Mean_Filt[i-1]) + (1 - alpha1)*Zero_Mean_Filt2[i-1]
+    end
+    return Zero_Mean_Filt
 end
 
-@doc """
+"""
+    ZeroMeanRoofingFilterK1(x::Array{Float64}; HPPeriod::Int64=48, Smooth::Int64=10)::Array{Float64}
+
+Zero Mean Roofing Filter - Lag 1 - Equation 7-2
+K1 = Lag 1
+"""
+function ZeroMeanRoofingFilterK1(x::Array{Float64}; HPPeriod::Int64=48, Smooth::Int64=10)::Array{Float64}
+    @assert HPPeriod<size(x,1) && HPPeriod>0 "Argument n out of bounds."
+    # Highpass filter cyclic components whose periods are shorter than 48 bars
+    alpha1 = (cosd(360 / HPPeriod) + sind(360 / HPPeriod) - 1) /cosd(360 / HPPeriod)
+    HP = zeros(x)
+    @inbounds for i = 2:size(x,1)
+        HP[i] = (1 - alpha1 / 2)*(x[i] - x[i-1]) +(1 - alpha1)*HP[i-1]
+    end
+    #Smooth with a Super Smoother Filter from equation 3-3
+    a1 = exp(-1.414*3.14159 / Smooth)
+    b1 = 2*a1*cosd(1.414*180 / Smooth)
+    c2 = b1
+    c3 = -a1*a1
+    c1 = 1 - c2 - c3
+    Zero_Mean_Filt = zeros(x)
+    Zero_Mean_Filt2 = zeros(x)
+    @inbounds for i = 3:size(x,1)
+        Zero_Mean_Filt[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*Zero_Mean_Filt[i-1] + c3*Zero_Mean_Filt[i-2]
+        Zero_Mean_Filt2[i] = (1 - alpha1 / 2)*(Zero_Mean_Filt[i] - Zero_Mean_Filt[i-1]) + (1 - alpha1)*Zero_Mean_Filt2[i-1]
+    end
+    return Zero_Mean_Filt2
+end
+
+"""
+    RoofingFilterIndicator(x::Array{Float64}; n1::Int64=40,n2::Int64=80)::Array{Float64}
+
 Roofing Filter As Indicator - Equation 7-3
-`RoofingFilterIndicator(x::Array{Float64}; n1::Int64=40,n2::Int64=80)::Array{Float64}`
 """
 function RoofingFilterIndicator(x::Array{Float64}; n1::Int64=40,n2::Int64=80)::Array{Float64}
-    @assert n<size(x,1) && n>0 "Argument n out of bounds."
-    # Highpass filter cyclic components whose periods are shorter than 48 bars
-LPPeriod = n1
-HPPeriod = n2
-alpha1 = (cosd(.707*360 / HPPeriod) + sind(.707*360 /HPPeriod) - 1) / cosd(.707*360 / HPPeriod)
-HP = zeros(x)
-@inbounds for i = 3:length(x)
+    @assert n1<size(x,1) && n1>0 "Argument n out of bounds."
+    # Highpass filter cyclic components whose periods are shorter than 48 (n) bars
+    LPPeriod = n1
+    HPPeriod = n2
+    alpha1 = (cosd(.707*360 / HPPeriod) + sind(.707*360 /HPPeriod) - 1) / cosd(.707*360 / HPPeriod)
+    HP = zeros(x)
+    @inbounds for i = 3:length(x)
     HP[i] = (1 - alpha1 / 2)*(1 - alpha1 / 2)*(x[i] - 2*x[i-1] + x[i-2]) + 2*(1 - alpha1)*HP[i-1] - (1 - alpha1)*(1 - alpha1)*HP[i-2]
-end
-#Smooth with a Super Smoother Filter from equation 3-3
-a1 = exp(-1.414*3.14159 / LPPeriod)
-b1 = 2*a1*cosd(1.414*180 / LPPeriod)
-c2 = b1
-c3 = -a1*a1
-c1 = 1 - c2 - c3
-Roof_filt_Indicator = zeros(x)
-@inbounds for i = 3:length(x)
-Roof_filt_Indicator[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*Roof_filt_Indicator[i-1] + c3*Roof_filt_Indicator[i-2]
-end
-return Roof_filt_Indicator
+    end
+    #Smooth with a Super Smoother Filter from equation 3-3
+    a1 = exp(-1.414*3.14159 / LPPeriod)
+    b1 = 2*a1*cosd(1.414*180 / LPPeriod)
+    c2 = b1
+    c3 = -a1*a1
+    c1 = 1 - c2 - c3
+    Roof_filt_Indicator = zeros(x)
+    @inbounds for i = 3:length(x)
+        Roof_filt_Indicator[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*Roof_filt_Indicator[i-1] + c3*Roof_filt_Indicator[i-2]
+    end
+    return Roof_filt_Indicator
 end
 
-@doc """
+"""
+    ModifiedStochastic(x::Array{Float64}; n::Int64=20)::Array{Float64}
+
 Modified Stochastic - Equation 7-4
-`ModifiedStochastic(x::Array{Float64}; n::Int64=20)::Array{Float64}`
 """
 function ModifiedStochastic(x::Array{Float64}; n::Int64=20)::Array{Float64}
     @assert n<size(x,1) && n>0 "Argument n out of bounds."
-#Highpass filter cyclic components whose periods are shorter than 48 bars
-alpha1 = (cosd(.707*360 / 48) + sind(.707*360 / 48) - 1) /cosd(.707*360 / 48)
-HP = zeros(x)
-@inbounds for i = 3:size(x,1)
-HP[i] = (1 - alpha1 / 2)*(1 - alpha1 / 2)*(x[i] - 2*x[i-1]+ x[i-2]) + 2*(1 - alpha1)*HP[i-1] - (1 - alpha1)*(1 -alpha1)*HP[i-2]
-end
-# Smooth with a Super Smoother Filter from equation 3-3
-a1 = exp(-1.414*3.14159 / 10)
-b1 = 2*a1*cosd(1.414*180 / 10)
-c2 = b1
-c3 = -a1*a1
-c1 = 1 - c2 - c3
-Filt = zeros(x)
-@inbounds for i = 3:size(x,1)
-Filt[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*Filt[i-1] + c3*Filt[i-2]
-end
-# Highest and lowest filt over n width
-HighestC = zeros(x)
-LowestC = zeros(x)
-Stoc = zeros(x)
-MyStochastic = zeros(x)
-@inbounds for i = n:size(x,1)
+    #Highpass filter cyclic components whose periods are shorter than 48 bars
+    alpha1 = (cosd(.707*360 / 48) + sind(.707*360 / 48) - 1) /cosd(.707*360 / 48)
+    HP = zeros(x)
+    @inbounds for i = 3:size(x,1)
+        HP[i] = (1 - alpha1 / 2)*(1 - alpha1 / 2)*(x[i] - 2*x[i-1]+ x[i-2]) + 2*(1 - alpha1)*HP[i-1] - (1 - alpha1)*(1 -alpha1)*HP[i-2]
+    end
+    # Smooth with a Super Smoother Filter from equation 3-3
+    a1 = exp(-1.414*3.14159 / 10)
+    b1 = 2*a1*cosd(1.414*180 / 10)
+    c2 = b1
+    c3 = -a1*a1
+    c1 = 1 - c2 - c3
+    Filt = zeros(x)
+    @inbounds for i = 3:size(x,1)
+        Filt[i] = c1*(HP[i] + HP[i-1]) / 2 + c2*Filt[i-1] + c3*Filt[i-2]
+    end
+    # Highest and lowest filt over n width
+    HighestC = zeros(x)
+    LowestC = zeros(x)
+    Stoc = zeros(x)
+    MyStochastic = zeros(x)
+    @inbounds for i = n:size(x,1)
     HighestC[i] = maximum(Filt[i-n+1:i])
     LowestC[i] = minimum(Filt[i-n+1:i])
     Stoc[i] = (Filt[i] - LowestC[i]) / (HighestC[i] - LowestC[i])
     MyStochastic[i] = c1*(Stoc[i] + Stoc[i-1]) / 2 + c2*MyStochastic[i-1] + c3*MyStochastic[i-2]
-end
-return MyStochastic
+    end
+    return MyStochastic
 end
 
 @doc """
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,11 @@ end
 # Validate against John Ehlers Original TradeStation Easylanguage code
 ######################################################################
 
+# Note - for some tests irratic results for the lead in period #
+
 # 3-3 SuperSmoother Test
 @testset "Ehlers" begin
-    @testset "3-3 SuperSmoother" begin
+    @testset "Super Smoother - Equation 3-3" begin
         filename = joinpath(dirname(@__FILE__), "test_3-3_Supersmoother.csv")
         test = CSV.read( filename)
         close = Float64.(test[:Close])
@@ -23,9 +25,10 @@ end
         valid = ifelse.(Super_Smoother .== super_smoother_benchmark,1,0)
         valid = valid[28:length(valid)] # remove indicator lead in period
         @test sum(valid) == length(valid)
+        # Passed but takes 28 lead in bars to do so
     end
 
-    @testset "4-1 Decycler" begin
+    @testset "Decycler - Equation 4-1" begin
         filename = joinpath(dirname(@__FILE__), "test_4-1_Decycler.csv")
         test = CSV.read( filename)
         close = Float64.(test[:Close])
@@ -36,16 +39,81 @@ end
         @test sum(valid) == length(valid)
     end
 
-    #=
-    @testset "4-2 - Decycle Oscillator" begin
+    @testset "Decycle Oscillator - Equation 4-2" begin
         filename = joinpath(dirname(@__FILE__), "test_4-2_Decycle_Oscillator.csv")
         test = CSV.read( filename)
+        close = Float64.(test[:Close])
         decycler_osc_benchmark = Float64.(test[:thirty_sixty_decycle_osc])
         decycler_osc = Decycle_OSC(close, n1=30,n2=60)
-        decycler_osc = round.(decycler,2) # round same as tradestation output
+        decycler_osc = round.(decycler_osc,2) # round same as tradestation output
         valid = ifelse.(decycler_osc .== decycler_osc_benchmark,1,0)
-        valid
-        @test sum(valid) == length(valid)
+        @test sum(valid) == length(valid)-148 # minus lead in
     end
-    =#
+
+    @testset "Band Pass Filter - Equation 5-1" begin
+        filename = joinpath(dirname(@__FILE__), "test_5-1_Band_Pass_Filter.csv")
+        test = CSV.read( filename)
+        close = Float64.(test[:Close])
+        BP_filt_benchmark = Float64.(test[:Trigger])
+        BP_filt = BandPassFilter(close,n=20,bandwidth=.3)
+        BP_filt = round.(BP_filt,2) # round same as tradestation output
+        valid = ifelse.(BP_filt .== BP_filt_benchmark,1,0)
+        @test sum(valid) == length(valid)-92  # 92 bar lead in period
+    end
+
+    @testset "Hurst Coefficient - Equation 6-1" begin
+        filename = joinpath(dirname(@__FILE__), "test_6-1_Hurst_Coefficient.csv")
+        test = CSV.read( filename)
+        close = Float64.(test[:Close])
+        Hurst_benchmark = Float64.(test[:Hurst_Coefficient])
+        Hurst = HurstCoefficient(close,n=30)
+        Hurst = round.(Hurst,4) # round same as tradestation output
+        valid = ifelse.(Hurst .== Hurst_benchmark,1,0)
+        @test sum(valid) == length(valid)-37 # 37 bar lead in period
+    end
+
+    @testset "HP LP Roofing Filter - Equation 7-1" begin
+        filename = joinpath(dirname(@__FILE__), "test_7-1_HP_LP_Roofing_Filter.csv")
+        test = CSV.read( filename)
+        close = Float64.(test[:Close])
+        HP_LP_Roof_benchmark = Float64.(test[:HP_LP_Roofing_Filter])
+        HP_LP_Roof = HPLPRoofingFilter(close)
+        HP_LP_Roof = round.(HP_LP_Roof,2) # round same as tradestation output
+        valid = ifelse.(HP_LP_Roof .== HP_LP_Roof_benchmark,1,0)
+        @test sum(valid) == length(valid)-48 # 48 bar lead in period
+    end
+
+    @testset "Zero Mean Roofing Filter - Lag 0 - Equation 7-2" begin
+        filename = joinpath(dirname(@__FILE__), "test_7-2_Zero_Mean_Roofing_Filter.csv")
+        test = CSV.read( filename)
+        close = Float64.(test[:Close])
+        Zero_Mean_Roofing_Filter_lag_0_benchmark = Float64.(test[:Filt])
+        Zero_Mean_Roofing_Filter_lag_0 = HPLPRoofingFilter(close)
+        Zero_Mean_Roofing_Filter_lag_0 = round.(Zero_Mean_Roofing_Filter_lag_0,2) # round same as tradestation output
+        valid = ifelse.(Zero_Mean_Roofing_Filter_lag_0 .== Zero_Mean_Roofing_Filter_lag_0_benchmark,1,0)
+        @test sum(valid) == length(valid)-48 # 48 bar lead in period
+    end
+
+    @testset "Zero Mean Roofing Filter - Lag 1 - Equation 7-2" begin
+        filename = joinpath(dirname(@__FILE__), "test_7-2_Zero_Mean_Roofing_Filter.csv")
+        test = CSV.read( filename)
+        close = Float64.(test[:Close])
+        Zero_Mean_Roofing_Filter_lag_1_benchmark = Float64.(test[:Filt2])
+        Zero_Mean_Roofing_Filter_lag_1 = ZeroMeanRoofingFilterK1(close)
+        Zero_Mean_Roofing_Filter_lag_1 = round.(Zero_Mean_Roofing_Filter_lag_1,2) # round same as tradestation output
+        valid = ifelse.(Zero_Mean_Roofing_Filter_lag_1 .== Zero_Mean_Roofing_Filter_lag_1_benchmark,1,0)
+        @test sum(valid) == length(valid)-60 # 48 bar lead in period
+    end
+
+    @testset "Roofing Filter Indicator Equation 7-3" begin
+        filename = joinpath(dirname(@__FILE__), "test_7-3_Roofing_Filter_Indicator.csv")
+        test = CSV.read( filename)
+        close = Float64.(test[:Close])
+        Roofing_Filter_Indicator_benchmark = Float64.(test[:Filt])
+        Roofing_Filter_Indicator = RoofingFilterIndicator(close)
+        Roofing_Filter_Indicator = round.(Roofing_Filter_Indicator,2) # round same as tradestation output
+        valid = ifelse.(Roofing_Filter_Indicator .== Roofing_Filter_Indicator_benchmark,1,0)
+        @test sum(valid) == length(valid)-213 # 213 bar lead in period
+    end
+
 end
