abaed28689845821c1943aede64f9070990cb0bf
Andrew Bannerman <bannerman1985@gmail.com>
2019-12-27 19:25:26 -0600

    runtests 1.2

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,11 @@ using MarketCycles
 using CSV
 using Statistics
 
-#@static if VERSION < v"0.7.0-DEV.2005"
-#    using Base.Test
-#else
-#    using Test
-#end
+@static if VERSION < v"0.7.0-DEV.2005"
+    using Base.Test
+else
+    using Test
+end
 
 ######################################################################
 # Validate against John Ehlers Original TradeStation Easylanguage code
