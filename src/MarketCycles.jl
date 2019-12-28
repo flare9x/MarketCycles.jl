326e392c894ef5ed8634623810a308a82e7fd03f
Andrew Bannerman <bannerman1985@gmail.com>
2019-12-27 18:21:01 -0600

    run tests for julia 1.2

--- src/MarketCycles.jl
+++ src/MarketCycles.jl
@@ -6,7 +6,7 @@ export
     SuperSmoother, Decycler, Decycle_OSC, BandPassFilter, DominantCycle, HurstCoefficient, HPLPRoofingFilter,
     ZeroMeanRoofingFilterK0, ZeroMeanRoofingFilterK1, RoofingFilterIndicator,
     ModifiedStochastic, ModifiedRSI, AutoCorrelationIndicator, SingleLagAutoCorrelationIndicator, AutoCorrelationPeriodogram,
-    AutoCorrelationReversals, DFTS, AdaptiveRSI, AdaptiveStochastic
+    AutoCorrelationReversals, DFTS, AdaptiveRSI, AdaptiveStochastic, PFish
 
     include("ehlers_cycles.jl")
 
